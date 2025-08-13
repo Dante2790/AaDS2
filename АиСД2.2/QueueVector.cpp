374e1a7e673c9b357241d1767ca23b2353c12598
“Tsvichenko <bussidan@gmail.com>
2025-08-14 01:51:28 +0300

    1.1.0

--- АиСД2.2/QueueVector.cpp
+++ АиСД2.2/QueueVector.cpp
@@ -1,1 +1,184 @@
 #include "QueueVector.h"
+
+template <class T>
+QueueVector<T>::QueueVector() :
+
+	currentSize(initialSize),
+	first(0),
+	last(0),
+	counter(0)
+{
+	try { data = new T[initialSize]; }
+	catch (std::bad_alloc er) {
+		std::cerr << "error: " << er.what() << '\n';
+	}
+};
+
+template <class T>
+void QueueVector<T>::resize(){
+	try {
+		bool flag = false;
+		int newCurrentSize = (this->currentSize) * 2;
+		T* newData = new T[newCurrentSize];
+		int i, j;
+		j = 0;
+		i = this->first;
+		if (i == 0) {
+			flag = true;
+		}
+		while (i < this->counter) {
+			newData[j] = this->data[i];
+			i++;
+			j++;
+		}
+		i = 0;
+		if (flag == false) {
+			while (i < this->last) {
+				newData[j] = this->data[i];
+				i++;
+				j++;
+			}
+		}
+
+		delete[] this->data;
+		this->data = newData;
+		this->currentSize = newCurrentSize;
+		this->first = 0;
+		this->last = this->counter;
+	}
+	catch (std::bad_alloc er) {
+		std::cerr << "error: " << er.what() << '\n';
+	}
+}
+template <class T>
+void QueueVector<T>::enQueue(const T& data) {
+	try {
+		if (this->counter < 0) {
+			throw UnexpectedError("Ungodly crap");
+		}
+		if (this->counter == currentSize) {
+			if (this->currentSize == maxSize) {
+				throw QueueOverflow("Queue overflow");
+			}
+			this->resize();
+		}
+		this->data[last] = data;
+		if (this->last == ((this->currentSize) - 1)) {
+			this->last = 0;
+		}
+		else {
+			this->last += 1;
+		}
+		this->counter++;
+
+	}
+	catch (UnexpectedError er) {
+		std::cerr << "error: " << er.what() << '\n';
+		std::cerr << "How????????????" << '\n';
+	}
+	catch (QueueOverflow er) {
+		std::cerr << "error: " << er.what() << '\n';
+	}
+
+}
+
+template <class T>
+T QueueVector<T>::deQueue() {
+	try {
+		if (this->isEmpty()) {
+			throw QueueUnderflow("Queue underflow");
+		}
+		T temp = this->data[first];
+		if (this->first == ((this->currentSize) - 1)) {
+			this->first = 0;
+		}
+		else {
+			this->first += 1;
+		}
+		this->counter--;
+		return temp;
+	}
+	catch (QueueUnderflow er) {
+		std::cerr << "error: " << er.what() << '\n';
+		return T{};
+	}
+}
+
+template <class T>
+void QueueVector<T>::print() {
+	try {
+		
+		if (this->isEmpty()) {
+			throw QueueUnderflow("Queue underflow");
+		}
+		bool flag = false;
+		
+		std::cout << "[";
+		for (int i = 0; i < this->currentSize; i++) {
+			if (this->counter == this->currentSize) {
+				flag = false;
+			}
+			if (((i % 10) == 0) && (i != this->currentSize) && (i != 0)) {
+				std::cout << '\n';
+			}
+			switch (flag) {
+			case true:
+				if (i < this->first) {
+					std::cout << '-' << ' ';
+				}
+				else {
+					if ((i + 1) == this->currentSize) {
+						std::cout << this->data[i];
+					}
+					else {
+						std::cout << this->data[i] << ' ';
+					}
+				}
+				break;
+			case false:
+				if (this->counter == this->currentSize) {
+					if ((i + 1) == this->currentSize) {
+						std::cout << this->data[i];
+					}
+					else {
+						std::cout << this->data[i] << ' ';
+					}
+				}
+				else {
+					if (((this->last) < (this->first)) && flag == false) {
+						if (((i + 1) == this->last)) {
+							flag = true;
+							std::cout << this->data[i] << ' ';
+						}
+						else if (((i == 0) && (this->last == 0))) {
+							flag = true;
+							std::cout << '-' << ' ';
+						}
+						else {
+							std::cout << this->data[i] << ' ';
+						}
+					}
+					else if (((this->last) > (this->first)) && ((i < this->last)) && (i >= this->first)) {
+						std::cout << this->data[i] << ' ';
+					}
+					else {
+						if ((i + 1) == this->currentSize) {
+							std::cout << '-';
+						}
+						else {
+							std::cout << '-' << ' ';
+						}
+					}
+				}
+				break;
+			}
+		}
+
+		std::cout << "]" << '	' << "Current size: " << this->currentSize << '\n';
+	}
+	catch (QueueUnderflow er) {
+		std::cerr << "error: " << er.what() << '\n';
+	}
+}
+
+template class QueueVector<int>;
--- АиСД2.2/QueueVector.h
+++ АиСД2.2/QueueVector.h
@@ -7,42 +7,91 @@
 
 template <class T>
 class QueueVector : public Queue<T> {
-private:
-	T data;
-	T* first;
-	T* last;
-	int initialSize;
-	int currentSize;
-	int count;
 
+///////
+private:
+	static const int initialSize = 10;
+	static const int maxSize = 80;
+
+	int first;
+	int last;
+	
+	int currentSize;
+	int counter;
+
+	T* data;
+	
+	/// <summary>
+	/// this method will double the queue size
+	/// </summary>
 	void resize();
-	bool isEmpty() override;
+
+	/// <summary>
+	/// this method will check if the queue is empty
+	/// </summary>
+	/// <returns>queue == empty ? true : false</returns>
+	bool isEmpty() override {
+		return this->counter == 0;
+	}
+
+	//////////////////////////////////////////////////
+	class QueueOverflow : public std::overflow_error {
+	private:
+		std::string message;
+	public:
+		explicit QueueOverflow(const std::string& msg) noexcept : std::overflow_error(msg), message(msg) {};
+		const char* what() const noexcept override {
+			return message.c_str();
+		}
+	};
+	class QueueUnderflow : public std::underflow_error {
+	private:
+		std::string message;
+	public:
+		explicit QueueUnderflow(const std::string& msg) noexcept : std::underflow_error(msg), message(msg) {};
+		const char* what() const noexcept override {
+			return message.c_str();
+		}
+	};
+	class UnexpectedError : public QueueUnderflow {
+	private:
+		std::string message;
+	public:
+		explicit UnexpectedError(std::string msg) noexcept : QueueUnderflow(msg), message(msg) {};
+		const char* what() const noexcept override {
+			return message.c_str();
+		}
+	};
+	//////////////////////////////////////////////////
+	
+///////
 public:
 	QueueVector();
-	~QueueVector();
+	~QueueVector() {
+		delete[] data;
+	};
+
+	/// <summary>
+	/// this method will output all the contents of the queue
+	/// </summary>
+	void print();
+	
+	/// <summary>
+	/// this method adds a new element to the ueue
+	/// </summary>
+	/// <param name="data">- element to add</param>
 	void enQueue(const T& data) override;
+
+	/// <summary>
+	/// this method will remove an element from the queue and return it's data
+	/// </summary>
+	/// <returns>element to delete and return</returns>
 	T deQueue() override;
 	
-
+	
 	
 };
 
-class QueueOverflow : public std::overflow_error {
-private:
-	std::string message;
-public:
-	QueueOverflow(std::string msg) : std::overflow_error(msg) {}
-	const char* what() const override {
-		return message.c_str();
-	}
-};
-class QueueUnderflow : public std::underflow_error {
-private:
-	std::string message;
-public:
-	QueueUnderflow(std::string msg) : std::underflow_error(msg) {}
-	const char* what() const override {
-		return message.c_str();
-	}
-};
+
+
 #endif;
